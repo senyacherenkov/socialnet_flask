d6dbec64487e838c8f20fc3f6dc13adeb332cfc3
senyacherenkov <senyacherenkov@yandex.ru>
2022-10-03 14:01:31 -0700

    social net works

---
+++ db_init.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS social_net;
+CREATE DATABASE social_net;
+USE social_net;
+-- GRANT USAGE ON *.* TO 'flask_server'@'localhost';
+DROP USER 'flask_server'@'localhost';
+FLUSH PRIVILEGES;
+CREATE USER 'flask_server'@'localhost' IDENTIFIED BY 'otus';
+GRANT ALL PRIVILEGES ON * . * TO 'flask_server'@'localhost';
+FLUSH PRIVILEGES;
+DROP TABLE IF EXISTS creds;
+DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS friends;
+CREATE TABLE creds (id INT unsigned NOT NULL AUTO_INCREMENT,login VARCHAR(150) NOT NULL, pwd VARCHAR(150) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE profile (id INT unsigned NOT NULL AUTO_INCREMENT, crid INT unsigned NOT NULL, fname VARCHAR(150) NOT NULL, sname VARCHAR(150) NOT NULL, age INT unsigned NOT NULL, gender VARCHAR(150) NOT NULL, hob VARCHAR(150) NOT NULL, city VARCHAR(150) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE friends (id INT unsigned NOT NULL AUTO_INCREMENT, userid INT unsigned NOT NULL, frid INT unsigned NOT NULL, PRIMARY KEY (id));
---
+++ sql.py
@@ -0,0 +1,54 @@
+import mysql.connector
+from mysql.connector import Error
+# import pandas as pd
+
+def create_db_connection(host_name, user_name, user_password, db_name = ""):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.commit()
+        print("Query {} connection successful".format(query))
+        return result
+    except Error as err:
+        print(f"Error: '{err}' query: {query}")
+
+
+def execute_file(connection, file):
+    cursor = connection.cursor()
+    try:
+        for line in open(file):
+            cursor.execute(line)
+            connection.commit()
+            print(line)
+    except Error as err:
+        print(f"Error: '{err}'")
+
+def execute_list_query(connection, sql, val):
+    cursor = connection.cursor()
+    try:
+        cursor.executemany(sql, val)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+# conn = create_db_connection("localhost", "root", "1")
+# execute_file(conn, "db_init.sql")
